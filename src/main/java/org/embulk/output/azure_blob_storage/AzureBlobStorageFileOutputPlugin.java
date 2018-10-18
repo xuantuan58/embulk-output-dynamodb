281cbb9fadb67ce33903cad6d779802e1813e7f2
sakama <satoshiakama@gmail.com>
2018-10-18 14:40:01 +0900

    Don't throw ConfigException even if fail to delete temp file

--- src/main/java/org/embulk/output/azure_blob_storage/AzureBlobStorageFileOutputPlugin.java
+++ src/main/java/org/embulk/output/azure_blob_storage/AzureBlobStorageFileOutputPlugin.java
@@ -222,10 +222,10 @@ public class AzureBlobStorageFileOutputPlugin
                                     log.info("Upload start {} to {}", file.getAbsolutePath(), filePath);
                                     blob.upload(new BufferedInputStream(new FileInputStream(file)), file.length());
                                     log.info("Upload completed {} to {}", file.getAbsolutePath(), filePath);
-                                    log.info("Delete completed local file {}", file.getAbsolutePath());
-                                    if (!file.delete()) {
-                                        throw new ConfigException("Couldn't delete local file " + file.getAbsolutePath());
+                                    if (file.exists() && !file.delete()) {
+                                        log.warn("Couldn't delete local file " + file.getAbsolutePath());
                                     }
+                                    log.info("Delete completed local file {}", file.getAbsolutePath());
                                     return null;
                                 }
 
